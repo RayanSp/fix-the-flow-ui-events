6d1ba2d24fabd40ee5c2e53166f14489d44a0f52
RayanSp <rayan.spall@hva.nl>
2022-12-12 17:00:11 +0100

    fixed current animation

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,9 +1,9 @@
 let interaction = document.querySelector('a:nth-of-type(12)')
 
-interaction.addEventListener('click', jumpHandler)
-interaction.addEventListener('animationend', jumpHandler)
+interaction.addEventListener('click', jump)
+interaction.addEventListener('animationend', jump)
 
-function jumpHandler() {
+function jump() {
   interaction.classList.toggle('jump')
 }
 
@@ -12,7 +12,7 @@ function jumpHandler() {
 let kleurverandering = document.querySelector('a:nth-of-type(1)')
 
 kleurverandering.addEventListener('click', colorchange)
-kleurverandering.addEventListener('animationend', colorchange)
+kleurverandering.addEventListener('transitionend', colorchange)
 
 function colorchange() {
   kleurverandering.classList.toggle('colorchange')
@@ -23,6 +23,7 @@ function colorchange() {
 let lettergrootte = document.querySelector('a:nth-of-type(2)')
 
 lettergrootte.addEventListener('click', fontsize)
+lettergrootte.addEventListener('transitionend', fontsize)
 
 function fontsize() {
   lettergrootte.classList.toggle('fontsize')
@@ -32,10 +33,10 @@ function fontsize() {
 
 let draaielement = document.querySelector('a:nth-of-type(3)')
 
-draaielement.addEventListener('click', spin)
-draaielement.addEventListener('animationend', spin)
+draaielement.addEventListener('click', spinelement)
+draaielement.addEventListener('transitionend', spinelement)
 
-function spin() {
+function spinelement() {
   draaielement.classList.toggle('spin')
 }
 
@@ -48,4 +49,16 @@ shakeelement.addEventListener('animationend', shake)
 
 function shake() {
   shakeelement.classList.toggle('shake')
+}
+
+
+// flip
+
+let flipelement = document.querySelector('a:nth-of-type(5)')
+
+flipelement.addEventListener('click', flip)
+flipelement.addEventListener('transitionend', flip)
+
+function flip() {
+  flipelement.classList.toggle('flip')
 }
